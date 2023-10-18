a38bdd7f5c2bd6334f908ee7253bf73a77558ab5
Yeshu grandhi <grandhiyeswanth6660@gmail.com>
2023-10-18 22:52:13 +0530

    restaurent

---
+++ menudata.service.js
@@ -0,0 +1,9 @@
+(function() {
+    angular.module('data')
+        .service('MenuDataService', MenuDataService);
+
+    MenuDataService.$inject = ['$http'];
+    function MenuDataService($http) {
+        // Implement the service as per the assignment instructions
+    }
+})();
---
+++ routes.js
@@ -0,0 +1,9 @@
+(function() {
+    angular.module('MenuApp')
+        .config(RoutesConfig);
+
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+        // Implement route configuration as per the assignment instructions
+    }
+})();
